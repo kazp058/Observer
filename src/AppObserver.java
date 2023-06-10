979da738f14e17f429196f47a112ad000b98e3d5
jcvivas <jcvivas@espol.edu.ec>
2023-06-10 10:43:53 -0500

    Creacion del fxml con la implementacion del star del AppObserver y la creacion de botones con sus respectivos onAction en el observerController

--- src/AppObserver.java
+++ src/AppObserver.java
@@ -1,4 +1,10 @@
+import java.io.IOException;
+
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 import javafx.stage.Stage;
 
 public class AppObserver extends Application {
@@ -9,7 +15,23 @@ public class AppObserver extends Application {
 
     @Override
     public void start(Stage primaryStage) {
-
+    	try {
+			FXMLLoader loader = new FXMLLoader(getClass().getResource("observer.fxml"));
+			Parent root = (Parent) loader.load();
+			observerController controller = loader.getController();
+			controller.setStage(primaryStage);
+			primaryStage.setScene(new Scene(root, 650,500));
+			primaryStage.setTitle("Tarea Aspectos");
+			primaryStage.show();
+//			root = FXMLLoader.load(getClass().getResource("observer.fxml"));
+//			primaryStage.setScene(new Scene(root, 650,500));
+//			primaryStage.setTitle("Tarea Aspectos");
+//	        primaryStage.show();
+		} catch (IOException e) {
+			System.out.println("Error al crear la ventana.");
+			e.printStackTrace();
+			
+		}
 
     }
 }
