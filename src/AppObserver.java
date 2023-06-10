33bca48a95a3c2a07317bed7c71ecb9e0fc4db20
Kevin Zhang <kazp058@gmail.com>
2023-06-10 09:48:47 -0500

    Libraries for javafx

--- src/AppObserver.java
+++ src/AppObserver.java
@@ -1,4 +1,15 @@
+import javafx.application.Application;
+import javafx.stage.Stage;
 
-public class AppObserver {
+public class AppObserver extends Application {
 
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+
+    }
 }
